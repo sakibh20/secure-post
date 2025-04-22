4b4cd4247c7a2d531cd34bda43cf696d38dc36f5
saurav_paul <sauravpaul.sunny@gmail.com>
2025-04-23 00:24:41 +0300

    Add websocket connection for lobby and user join

---
+++ Backend/GameService/routers/websocket_routes.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, WebSocket
+from services.lobby import handle_lobby_socket, handle_user_socket
+
+websocket_router = APIRouter()
+
+
+@websocket_router.websocket("/ws/lobby")
+async def lobby_socket(websocket: WebSocket):
+    await handle_lobby_socket(websocket)
+
+
+@websocket_router.websocket("/ws/user/{user_id}")
+async def user_socket(websocket: WebSocket, user_id: str):
+    await handle_user_socket(websocket, user_id)
