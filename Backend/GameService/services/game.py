567b35f6b75ac39225f38105e55f76a6029e8de0
saurav_paul <sauravpaul.sunny@gmail.com>
2025-04-24 23:36:02 +0300

    Create a game

--- Backend/GameService/services/game.py
+++ Backend/GameService/services/game.py
@@ -1,6 +1,8 @@
 from services.commands import request_match_command
 from services.common import notify_user
-from services.models import MatchCreateRequest
+from services.data import game_data
+from services.game_manager import GameManager
+from services.models import MatchCreateRequest, MatchAcceptRequest, Game, GameStatus, GameState, GameProcess
 
 
 async def match_request_notifier(payload: MatchCreateRequest):
@@ -9,3 +11,18 @@ async def match_request_notifier(payload: MatchCreateRequest):
         "requested_by": payload.Player1,
         "match_id": payload.MatchId,
     }))
+
+
+async def create_game(payload: MatchAcceptRequest):
+    game = Game(
+        matchId=payload.MatchId,
+        player1=payload.Player1,
+        player2=payload.Player2,
+        status=GameStatus.WAITING,
+        matchToken=payload.MatchToken,
+        currentState=GameState(
+            currentTurn=payload.Player1,
+            currentProcess=GameProcess.ROLLING,
+        )
+    )
+    game_data[payload.MatchId] = game
