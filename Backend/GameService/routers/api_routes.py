114597b6afe1591bbbf0c905a20fb90cda2de2ff
saurav_paul <sauravpaul.sunny@gmail.com>
2025-04-24 22:52:26 +0300

    Add match request api

--- Backend/GameService/routers/api_routes.py
+++ Backend/GameService/routers/api_routes.py
@@ -1,26 +1,40 @@
 import random
 
-from fastapi import APIRouter, Body
-from starlette.responses import JSONResponse
+from fastapi import APIRouter
+
+from services.common import notify_user
+from services.game import match_request_notifier
+from services.models import DiceRollRequest, ClaimRequest, MatchCreateRequest, MatchAcceptRequest
 
 api_router = APIRouter()
 
 
 @api_router.post("/roll-dice")
-async def roll_dice(payload: dict = Body(...)):
-    match_id = payload.get("match_id")
-    user_id = payload.get("user_id")
-
-    if not match_id or not user_id:
-        return JSONResponse(status_code=400, content={
-            "error": "match_id and user_id are required"
-        })
-
+async def roll_dice_api(payload: DiceRollRequest):
     value = random.randint(1, 6)
-    print(f"User {user_id} rolled a {value} in match {match_id}")
-
+    print(f"User {payload.user_id} rolled a {value} in match {payload.match_id}")
+    await notify_user(payload.user_id)
     return {
-        "match_id": match_id,
-        "user_id": user_id,
+        "match_id": payload.match_id,
+        "user_id": payload.user_id,
         "dice_roll": value
     }
+
+
+@api_router.post("/claim")
+async def claim_dice_api(payload: ClaimRequest):
+    pass
+
+
+@api_router.post("/match/request")
+async def match_request_api(payload: MatchCreateRequest):
+    await match_request_notifier(payload)
+    return {
+        "detail": "Match request sent",
+    }
+
+
+@api_router.post("/match/accept")
+async def match_accept_api(payload: MatchAcceptRequest):
+    # await create_match()
+    pass
