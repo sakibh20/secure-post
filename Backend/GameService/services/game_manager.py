7db536896b4f771a5061a1a23bf0c00059637ae5
saurav_paul <sauravpaul.sunny@gmail.com>
2025-04-25 03:27:27 +0300

    Implement full game logic with notification of game start, round over and match over

--- Backend/GameService/services/game_manager.py
+++ Backend/GameService/services/game_manager.py
@@ -11,7 +11,11 @@ class GameManager:
 
     def is_game_over(self):
         """Check if the game is over."""
-        if self.game.currentRound == self.game.totalRound and self.game.status == GameStatus.FINISHED:
+        if (
+                (self.game.currentRound > self.game.totalRound)
+                or (max(self.game.player1Score, self.game.player2Score) > self.game.totalRound // 2)
+        ):
+            self.game.status = GameStatus.FINISHED
             return True
         return False
 
@@ -20,6 +24,10 @@ class GameManager:
 
         if self.is_game_over():
             raise ValueError("Game is already over.")
+
+        if self.game.status == GameStatus.WAITING:
+            self.game.status = GameStatus.IN_PROGRESS
+            await self.notify_match_start()
 
         game = self.game
         state = game.currentState
@@ -37,7 +45,6 @@ class GameManager:
                 player=state.currentTurn,
             )
         elif state.currentProcess == GameProcess.ROUND_OVER:
-            round_winner = None
             if bool(state.claim == state.roll) == state.decide:
                 if state.currentTurn == self.game.player1:
                     self.game.player1Score += 1
@@ -57,6 +64,8 @@ class GameManager:
             raise ValueError("Invalid game process.")
 
         await self.update_game_state()
+        if self.is_game_over():
+            await self.notify_game_over()
         print(self.game.model_dump_json(indent=2))
 
     async def handle_rolling(self, player):
@@ -80,7 +89,9 @@ class GameManager:
         await self.send_command(
             player,
             "decide",
-            {}
+            {
+                "claim": self.game.currentState.claim,
+            }
         )
 
     async def send_command(self, player, command, payload):
@@ -91,17 +102,55 @@ class GameManager:
             "payload": payload
         })
 
-    async def notify_game_status(self, current_state):
+    async def notify_game_over(self):
+        for player in [self.game.player1, self.game.player2]:
+            if self.game.player1Score > self.game.player2Score:
+                winner = self.game.player1
+            elif self.game.player1Score < self.game.player2Score:
+                winner = self.game.player2
+            else:
+                winner = None
+            if winner is None:
+                win_status = "draw"
+            else:
+                win_status = "win" if winner == player else "lose"
+            await self.send_command(player, "game_over", {
+                "status": str(self.game.status),
+                "player1_score": self.game.player1Score,
+                "player2_score": self.game.player2Score,
+                "player1": self.game.player1,
+                "player2": self.game.player2,
+                "game_status": win_status,
+                "current_round": self.game.currentRound,
+                "total_round": self.game.totalRound,
+            })
+
+    async def notify_match_start(self):
+        for player in [self.game.player1, self.game.player2]:
+            await self.send_command(player, "match_start", {
+                "status": str(self.game.status),
+                "player1_score": self.game.player1Score,
+                "player2_score": self.game.player2Score,
+                "player1": self.game.player1,
+                "player2": self.game.player2,
+                "current_round": self.game.currentRound,
+                "total_round": self.game.totalRound,
+            })
+
+    async def notify_round_over(self, current_state):
         for player in [self.game.player1, self.game.player2]:
             await self.send_command(player, "round_over", {
                 "round_winner": current_state.round_winner,
                 "round_status": "win" if current_state.round_winner == player else "lose",
-                "game_status": str(self.game.status),
+                "status": str(self.game.status),
                 "player1_score": self.game.player1Score,
                 "player2_score": self.game.player2Score,
+                "player1": self.game.player1,
+                "player2": self.game.player2,
                 "current_round": self.game.currentRound,
                 "total_round": self.game.totalRound,
             })
+
     async def update_game_state(self):
         """Update the game state based on the current process."""
         current_state = self.game.currentState.currentProcess
@@ -121,8 +170,7 @@ class GameManager:
                 currentProcess=GameProcess.ROLLING,
             )
             self.game.currentRound += 1
-            await self.notify_game_status(current_state)
-
+            await self.notify_round_over(current_state)
 
     def get_opponent(self):
         """Get the opponent of the current player."""
